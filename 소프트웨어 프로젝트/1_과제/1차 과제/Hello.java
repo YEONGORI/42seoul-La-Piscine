21e4746a7fabc946484e0c47706b065a0806f3d8
YEONGORI <dusrjf14@gmail.com>
2022-04-05 12:39:57 +0900

    get GCD

--- 소프트웨어 프로젝트/1_과제/1차 과제/Hello.java
+++ 소프트웨어 프로젝트/1_과제/1차 과제/Hello.java
@@ -4,20 +4,84 @@ public class Hello {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] iNumbers = new int[1024];
+        int iNumbers[] = new int[1000];
+        int primeFactors[][][] = new int[1000][2][100];
+        int GCD = 1, LCN = 1;
 
         for (int i = 0; true; i++) {
-            System.out.print("Input : ");
+            System.out.print("\nInput : ");
             iNumbers[i] = scanner.nextInt();
-            PrimeFactorization(iNumbers);
-            System.out.printf("Input Numbers : %d", iNumbers[i]);
+            if (iNumbers[i] == 0) {
+                break;
+            }
 
+            primeFactors = primeFactorization(iNumbers[i], i);
+            GCD = getGCD(primeFactors, iNumbers[i], i);
+
+            System.out.print("Input Numbers : ");
+            for (int j = -1; j < i; j++) {
+                System.out.printf("%d ", iNumbers[j + 1]);
+            }
+
+            System.out.printf("\nLCN : %d", LCN);
+            System.out.printf("\nGCD : %d", GCD);
         }
 
-        System.out.printf("LCN : %d", )
+        scanner.close();
     }
 
-    public static void PrimeFactorization(int[] iNumbers) {
+    public static int[][][] primeFactorization(int iNumber, int iNumberCnt) {
+        int primeFactors[][][] = new int[1000][2][100];
+        int exponent, cnt = 0;
 
+        primeFactors[iNumberCnt][0][cnt] = 1;
+        primeFactors[iNumberCnt][1][cnt] = 1;
+
+        for (exponent = 0; iNumber % 2 == 0; exponent++) {
+            iNumber /= 2;
+        }
+
+        cnt++;
+        primeFactors[iNumberCnt][0][cnt] = 2;
+        primeFactors[iNumberCnt][1][cnt] = exponent;
+
+        for (int divNumber = 3; iNumber != 1; divNumber += 2) {
+            cnt++;
+            for (exponent = 0; iNumber % divNumber == 0; exponent++) {
+                iNumber /= divNumber;
+            }
+            primeFactors[iNumberCnt][0][cnt] = divNumber;
+            primeFactors[iNumberCnt][1][cnt] = exponent;
+        }
+
+        return primeFactors;
+    }
+
+    public static int getGCD(int primeFactors[][][], int iNumber, int iNumberCnt) {
+        int maxFactor = 1;
+        int cnt = 0;
+        int minExponent;
+        int j = 0;
+        for (int i = 0; i < 9; i++) {
+            minExponent = 1000;
+            j = 0;
+
+            if (primeFactors[j][1][i] > 0) {
+                for (j = 0; j < iNumberCnt; j++) {
+                    if (primeFactors[j][1][i] > 0) {
+                        if (primeFactors[j][1][i] < minExponent) {
+                            minExponent = primeFactors[j][1][i];
+                        }
+                        cnt++;
+                    }
+                }
+            }
+
+            if (cnt == iNumberCnt) {
+                maxFactor *= Math.pow(primeFactors[0][0][i], minExponent);
+            }
+        }
+
+        return maxFactor;
     }
 }
